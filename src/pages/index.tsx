6223b7aaca61aace699079c72a01d1db48df576d
Shuhei <sh7916@gmail.com>
2020-04-24 16:02:20 +0900

    Rename components/NewNoteView to NewNote

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import NewNoteView from '~/components/NewNoteView.tsx'
+import NewNote from '~/components/NewNote'
 
 export default function Home() {
   return (
@@ -10,7 +10,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <NewNoteView />
+        <NewNote />
       </main>
 
       <footer>
