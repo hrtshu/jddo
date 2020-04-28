1d04966e7230b4828bb18fbd0856a4963f13c2f9
Shuhei <sh7916@gmail.com>
2020-04-28 10:17:16 +0900

    Convert components/Home to class component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,43 @@
+import React from 'react'
 import Head from 'next/head'
-import { NewNote, NoteList } from '~/components'
 import { Box } from '@material-ui/core'
+import { NewNote, NoteList } from '~/components'
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Jddo</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export interface HomeProps {}
 
-      <main>
-        <Box display="flex" justifyContent="center" m={2}>
-          <Box width={1} maxWidth="700px">
-            <Box display="flex" justifyContent="center">
-              <NewNote width={0.7} maxWidth="400px" mb={2} onCreateButtonClick={(subject: string, body: string) => {console.log('New note created:', subject, body)}} />
+interface State {}
+
+export class Home extends React.Component<HomeProps, State> {
+  render() {
+    return (
+      <div className="container">
+        <Head>
+          <title>Jddo</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+  
+        <main>
+          <Box display="flex" justifyContent="center" m={2}>
+            <Box width={1} maxWidth="700px">
+              <Box display="flex" justifyContent="center">
+                <NewNote width={0.7} maxWidth="400px" mb={2} onCreateButtonClick={(subject: string, body: string) => {console.log('New note created:', subject, body)}} />
+              </Box>
+              <NoteList />
             </Box>
-            <NoteList />
           </Box>
-        </Box>
-      </main>
-
-      <footer>
-      </footer>
-
-      <style jsx>{`
-      `}</style>
-
-      <style jsx global>{`
-      `}</style>
-    </div>
-  )
+        </main>
+  
+        <footer>
+        </footer>
+  
+        <style jsx>{`
+        `}</style>
+  
+        <style jsx global>{`
+        `}</style>
+      </div>
+    )
+  }
 }
+
+export default Home
