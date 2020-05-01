7df5654ba8509a7a64bfcb87132aaebfd150c87d
Shuhei <sh7916@gmail.com>
2020-05-01 18:27:47 +0900

    Implement logic to fetch note data from server
    Reference: https://www.apollographql.com/blog/apollo-client-now-with-react-hooks-676d116eeae2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,58 +1,61 @@
 import React from 'react'
 import Head from 'next/head'
 import { Box } from '@material-ui/core'
+import { useQuery } from '@apollo/react-hooks'
+import { gql } from 'apollo-boost'
 import * as types from '~/types'
 import { NewNote, NoteList } from '~/components'
 
 export interface HomeProps {}
 
-interface State {
-  notes: types.Note[]
-}
-
-export class Home extends React.Component<HomeProps, State> {
-  constructor(props: HomeProps) {
-    super(props)
-    this.state = {
-      notes: []
-    }
-  }
-  onCreateButtonClick = (note: types.Note) => {
+const Home = (props: HomeProps) => {
+  const onCreateButtonClick = (note: types.Note) => {
     this.setState({
       notes: [ note, ...this.state.notes ]
     })
     console.log('New note created:', note.subject, note.body)
   }
-  render() {
-    return (
-      <div className="container">
-        <Head>
-          <title>Jddo</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
   
-        <main>
-          <Box display="flex" justifyContent="center" m={2}>
-            <Box width={1} maxWidth="700px">
-              <Box display="flex" justifyContent="center">
-                <NewNote width={0.7} maxWidth="400px" mb={2} onCreateButtonClick={this.onCreateButtonClick} />
-              </Box>
-              <NoteList notes={this.state.notes} />
+  const query = gql`
+    query {
+      notes {
+        id,
+        subject,
+        body
+      }
+    }
+  `
+  const {loading, error, data } = useQuery(query)
+  console.log(data)
+
+  return (
+    <div className="container">
+      <Head>
+        <title>Jddo</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Box display="flex" justifyContent="center" m={2}>
+          <Box width={1} maxWidth="700px">
+            <Box display="flex" justifyContent="center">
+              <NewNote width={0.7} maxWidth="400px" mb={2} onCreateButtonClick={onCreateButtonClick} />
             </Box>
+            { loading ? 'Loading...' : <NoteList notes={data.notes} /> }
           </Box>
-        </main>
-  
-        <footer>
-        </footer>
-  
-        <style jsx>{`
-        `}</style>
-  
-        <style jsx global>{`
-        `}</style>
-      </div>
-    )
-  }
+        </Box>
+      </main>
+
+      <footer>
+      </footer>
+
+      <style jsx>{`
+      `}</style>
+
+      <style jsx global>{`
+      `}</style>
+    </div>
+  )
 }
 
 export default Home
