3d44b71e3b788dbfad243f383f3d31a6fa7f0016
Shuhei <sh7916@gmail.com>
2020-05-07 13:34:55 +0900

    Add AppBar and loading spinner
    Reference:
    - https://material-ui.com/components/app-bar/
    - http://yucatio.hatenablog.com/entry/2018/12/12/083806

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Head from 'next/head'
-import { Box, Snackbar } from '@material-ui/core'
+import { Box, Snackbar, CircularProgress, AppBar, Toolbar, IconButton, Typography, makeStyles, createStyles, Theme, CssBaseline } from '@material-ui/core'
 import { Alert } from '@material-ui/lab'
+import { Menu as MenuIcon, Done as DoneIcon } from '@material-ui/icons'
 import { useQuery, useMutation } from '@apollo/react-hooks'
 import { gql } from 'apollo-boost'
 import * as types from '~/types'
 import { NewNote, NoteList } from '~/components'
-
-export interface HomeProps {}
   
 const FETCH_NOTES = gql`
   query {
@@ -30,16 +29,31 @@ const CREATE_NOTE = gql`
     }
   }
 `
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+    toolbar: theme.mixins.toolbar
+  }),
+)
 
-const Home = (props: HomeProps) => {
+const Home = () => {
   const onCreateButtonClick = (note: types.Note) => {
     createNote({ variables: { subject: note.subject, body: note.body } })
     console.log('New note created:', note.subject, note.body)
   }
   
-  const [createNote, res] = useMutation(CREATE_NOTE)
-  const { loading, error, data } = useQuery(FETCH_NOTES)
-  // TODO resのloadingやerrorのハンドリング
+  const classes = useStyles();
+  const [createNote, { loading: mutationLoading, error: mutationError }] = useMutation(CREATE_NOTE)
+  const { loading: queryLoading, error: queryError, data } = useQuery(FETCH_NOTES)
+  // TODO mutationErrorのハンドリング
 
   return (
     <div className="container">
@@ -48,18 +62,31 @@ const Home = (props: HomeProps) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className={classes.root}>
+        <AppBar position="fixed">
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              JDDO
+            </Typography>
+            { queryLoading || mutationLoading ? <CircularProgress size={30} color="secondary" /> : <DoneIcon /> }
+          </Toolbar>
+        </AppBar>
+        
+        <Box className={classes.toolbar} /> {/* AppBarはfixedされているのでその分だけ高さを下げる用 */}
         <Box display="flex" justifyContent="center" m={2}>
           <Box width={1} maxWidth="700px">
             <Box display="flex" justifyContent="center">
               <NewNote width={0.7} maxWidth="400px" mb={2} onCreateButtonClick={onCreateButtonClick} />
             </Box>
-            <Snackbar open={!!error}>
-              <Alert severity="error">{ /* TODO エラー内容に応じて分岐 */ }
+            <Snackbar open={!!queryError}>
+              <Alert severity="error"> { /* TODO エラー内容に応じて分岐 */ }
                 サーバとの接続に失敗しました。リロードしてください。
               </Alert>
             </Snackbar>
-            { error ? "" : (loading ? 'Loading...' : <NoteList notes={data.notes} />) }
+            { queryError || queryLoading ? "" : <NoteList notes={data.notes} /> }
           </Box>
         </Box>
       </main>
