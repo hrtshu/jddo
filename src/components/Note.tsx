5b4cf6a8e9aea926fb924fcdd20eb4aae9c016a5
Shuhei <sh7916@gmail.com>
2020-05-18 16:02:01 +0900

    Convert components/Note to Function Component

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,53 +1,44 @@
 import * as React from 'react'
-import TextField from '@material-ui/core/TextField'
-import { Box, BoxProps } from '@material-ui/core'
+import { Box, BoxProps, TextField } from '@material-ui/core'
 import * as types from '~/types'
 
 export type NoteProps = {
   note: types.Note
 } & types.OnNoteChange & types.ReadOnly & BoxProps
 
-interface State {}
-
-export class Note extends React.Component<NoteProps, State> {
-  static defaultProps = {
-    readOnly: false
-  }
-  onSubjectChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (this.props.onNoteChange) {
-      let newNote = this.props.note.clone()
+export const Note = ({ note, onNoteChange, readOnly = false, ...otherProps }: NoteProps) => {
+  const onSubjectChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (onNoteChange) {
+      let newNote = note.clone()
       newNote.subject = e.currentTarget.value
-      this.props.onNoteChange(newNote)
+      onNoteChange(newNote)
     }
   }
-  onBodyChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (this.props.onNoteChange) {
-      let newNote = this.props.note.clone()
+  const onBodyChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (onNoteChange) {
+      let newNote = note.clone()
       newNote.body = e.currentTarget.value
-      this.props.onNoteChange(newNote)
+      onNoteChange(newNote)
     }
   }
-  render() {
-    const { note, onNoteChange, readOnly, ...otherProps } = this.props
-    return (
-      <Box width={1} border={1} borderColor="primary.main" borderRadius={5} {...otherProps}>
-        <TextField
-          fullWidth
-          value={note.subject}
-          onChange={this.onSubjectChange}
-          InputProps={{ readOnly }}
-        />
-        <TextField
-          multiline
-          rows={5}
-          fullWidth
-          value={note.body}
-          onChange={this.onBodyChange}
-          InputProps={{ readOnly }}
-        />
-      </Box>
-    )
-  }
+  return (
+    <Box width={1} border={1} borderColor="primary.main" borderRadius={5} {...otherProps}>
+      <TextField
+        fullWidth
+        value={note.subject}
+        onChange={onSubjectChange}
+        InputProps={{ readOnly }}
+      />
+      <TextField
+        multiline
+        rows={5}
+        fullWidth
+        value={note.body}
+        onChange={onBodyChange}
+        InputProps={{ readOnly }}
+      />
+    </Box>
+  )
 }
 
 export default Note
